6bc540ab656fb178b179041dbf2d086f2af41107
Newton <newtonkirimi55@gmail.com>
2024-07-01 03:08:35 -0700

    Looping Lab (CodeGrade)

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+// // For Loop: Wrap Gifts
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+// For Loop: Write Cards
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+// While Loop: Count Down
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
